ea7911ce22061635e01141d3305b4b8196716b8b
Ahmadullah Nawrozy <anawrozy2021@gmail.com>
2023-11-15 21:31:17 -0500

    jdbc examples

---
+++ Employee.java
@@ -0,0 +1,11 @@
+package Dailypractice;
+
+public class Employee extends Test {
+
+	public static void main(String[] args) {
+		Test = new Test();
+		
+		System.out.println("data is coming from Test class" + obj.accountNumber);
+	}
+	
+}
